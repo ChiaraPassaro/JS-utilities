69c70f27271c38ba2f9e7752f6fc0b76340c62f2
Chiara Passaro <chiarapassaro.wd@gmail.com>
2019-06-28 20:28:50 +0200

    Mod maxNum minNum add  qs(selector) / qsa(selector) add isArray check

--- utilities.js
+++ utilities.js
@@ -3,11 +3,10 @@ Funzione che ritorna il numero minore
 accetta un array o una serie di argomenti
 */
 function minNumber(){
-    if (typeof arguments[0] == 'object' ){
-        var result = Math.min.apply(null, arguments[0]);
-    } else {
-        var result = Math.min.apply(null, arguments);
-    }    return result;
+    if (Object.prototype.toString.call(arguments[0]) === '[object Array]'){
+        return Math.min.apply(null, arguments[0]);
+    }
+    return Math.min.apply(null, arguments);
 }
 
 /*
@@ -15,12 +14,10 @@ Funzione che ritorna il numero maggiore
 accetta un array o una serie di argomenti
 */
 function maxNumber(){
-    if (typeof arguments[0] == 'object' ){
-        var result = Math.max.apply(null, arguments[0]);
-    } else {
-        var result = Math.max.apply(null, arguments);
+    if (Object.prototype.toString.call(arguments[0]) === '[object Array]'){
+        return Math.max.apply(null, arguments[0]);
     }
-    return result;
+    return Math.max.apply(null, arguments);
 }
 
 /*
@@ -28,7 +25,6 @@ Funzione che chiede un numero attraverso un prompt
 richiesta viene rieffettuata finche' l'utente non inserisce un numero
 */
 function askNumber(messaggio) {
-    var i = 0;
     var boolean = false;
 
     while (!boolean){
@@ -47,26 +43,51 @@ function askNumber(messaggio) {
 
 //Funzione che genera un numero random
 function getRandom(min, max){
-    var random = Math.floor(Math.random() * (max - min + 1)+ min);
-    return random;
+    return Math.floor(Math.random() * (max - min + 1)+ min);
 }
 
 //Funzione che controlla se un numero rientra in uno specifico range estremi compresi
 function checkNumberFromTo (num, min, max){
-    var result = false;
     if(num >= min && num <= max) {
-        result = true;
+        return true;
     }
-    return result;
+    return false;
 }
 
 //Funzione che controlla se un numero è pari ritorna booleano
 function isEven(number) {
-    var even = false;
-    var number = number;
-
-    if (number % 2 == 0){
-        even = true;
+    if (number % 2 === 0){
+        return true;
     }
-    return even;
+    return false;
+}
+
+/************
+ *************
+ Il metodo querySelector() restituisce il primo elemento discendente dell'elemento su cui è invocato corrispondente al gruppo specificato di selettori.
+ *************
+ ************/
+
+function qs(selector) {
+    return document.querySelector(selector);
+}
+
+/************
+*************
+Il metodo querySelectorAll() restituisce una NodeList statica.
+È possibile specificare più selettori separandoli utilizzando le virgole.
+Una volta restituita la NodeList degli elementi di corrispondenza, è possibile esaminarlo come qualsiasi array. Se la matrice è vuota (ovvero la sua proprietà length è 0), non è stata trovata alcuna corrispondenza.
+
+Attenzione NodeList non è un vero array, vale a dire che non ha i metodi dell'array come slice, some, map etc.
+ *************
+ ************/
+
+function qsa(selector) {
+    return document.querySelectorAll(selector);
+}
+
+
+// http://perfectionkills.com/instanceof-considered-harmful-or-how-to-write-a-robust-isarray/
+function isArray(param) {
+    return Object.prototype.toString.call(param) === '[object Array]';
 }
